4dea607cd6a56370e40e76c52769dc8d9df1196a
Valhndsm <114047905+valhndsm@users.noreply.github.com>
2024-08-30 14:06:13 +0800

    Update proposalv2.js

--- proposalv2.js
+++ proposalv2.js
@@ -4,6 +4,25 @@ document.oncontextmenu = () => {
 	return false
 }
 
+document.onkeydown = e => {
+	if (e.key == "F12") {
+	alert("Don't try to inspect element")
+	return false
+}
+	if(e.ctrlKey && e.key == "u"){
+		alert("Don't try to view page sources")
+		return false
+	}
+	if (e.ctrlKey && e.key == "c"){
+		alert("Don't try to copy page element")
+		return false
+	}
+	if(e.ctrlKey && e.key == "v"){
+		alert("Don't try to paste anything to page")
+		return false
+	}
+}
+
 const comeback = document.getElementById("btn2");
 const yes = document.getElementById("btn1");
 
